3cc33241915e652f12872ee098686dce95c5b0bd
Eliaslvr <eliaslvr59@gmail.com>
2024-01-02 17:20:06 +0300

    Avancement modale

--- FrontEnd/assets/js/modale.js
+++ FrontEnd/assets/js/modale.js
@@ -20,8 +20,13 @@ modale.appendChild(modaleElement)
 let inModale = document.createElement('p');
 inModale.classList.add('inModale');
 modaleElement.appendChild(inModale);
-inModale.innerHTML = `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam hic quae ad et ipsa, consequuntur ipsum similique incidunt voluptates sequi quos aut numquam vero magni optio deserunt a. Eos, voluptatibus.`
+// inModale.innerHTML = `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam hic quae ad et ipsa, consequuntur ipsum similique incidunt voluptates sequi quos aut numquam vero magni optio deserunt a. Eos, voluptatibus.`
 
+let titleModale = document.createElement('h2');
+titleModale.classList.add('titleModale');
+let inModaleClass = document.querySelector('.inModale')
+inModaleClass.appendChild(titleModale);
+titleModale.innerHTML = "Galerie photo";
 
 projetH2.addEventListener("click", (e) => {
     e.preventDefault();   
@@ -42,3 +47,73 @@ window.addEventListener('click', function (event) {
         modale.style.display = 'none';
     }
 });
+
+
+
+//Ajouter element à l'interieur du modale
+// const fetchLog = fetch('http://localhost:5678/api/works')
+// .then(data => data.json())
+// .then(data => {
+//     return `<figure>
+//         <div data-category=${data.categoryId}></div>
+//         <img src=${data.imageUrl} alt=${data.title}>
+//         <figcaption>${data.title}</figcaption>
+//     </figure>`
+// })
+
+
+
+
+function fetchAffiche(){ 
+    async function fetchWorks() {
+    let response = await fetch("http://localhost:5678/api/works")
+    return await response.json();
+}
+
+let works = [];
+
+async function buildGallery(works) {
+   for(let affiche of works) {
+    inModale.innerHTML += 
+    `<figure class='modaleElementAffiche'>
+        <img src=${affiche.imageUrl} alt=${affiche.title}>
+    </figure>`
+   }
+};
+
+document.addEventListener('DOMContentLoaded', async function() {
+    works = await fetchWorks()
+    buildGallery(works)
+})
+}
+
+
+function buttonModale() {
+    let buttonAjout = document.createElement('button');
+    buttonAjout.innerHTML = 'Ajouter une photo'
+    buttonAjout.classList.add('buttonAjout');
+    inModale.appendChild(buttonAjout)
+}
+
+
+async function render() {
+    const skeletun = `
+    <div>
+        <section>
+            ${fetchAffiche()}
+        </section>
+        <section>
+            ${buttonModale()}
+        </section>
+    </div>`
+    append(skeletun)
+}
+
+render()
+
+
+
+
+
+// Insertion du bouton après l'élément avec la classe "modaleElementAffiche"
+//modaleElementAffiche.parentNode.insertBefore(buttonAjout, modaleElementAffiche.nextSibling);
